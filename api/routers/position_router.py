569fe0795f8ddf692c9b76e9c5d632904684c753
eslunap19 <estefany.luna@gmail.com>
2021-01-07 17:05:28 -0500

    creation files in router/ employee, position & service_order_detail

---
+++ api/routers/position_router.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, APIRouter, HTTPException
+from sqlalchemy.orm import Session
+from api.db.db_connection import get_db
+from api.db.position_db import PositionInDB
+from api.models.position_models import PositionIn, PositionOut
+
+router = APIRouter()
+
+@router.get("/service/{position_id}", response_model=PositionOut)
+async def get_position(position_id: int, db: Session = Depends(get_db)):
+    position_in_db = db.query(PositionInDB).get(position_id)
+    if position_in_db == None:
+        raise HTTPException(status_code=404, detail="El cargo solicitado no existe.")
+    return position_in_db
+
+@router.post("/employee/position/")
+async def new_position(position_in: PositionIn, db: Session = Depends(get_db)):
+
+    position_in_db = PositionInDB(**position_in.dict())
+
+    db.add(position_in_db)
+    db.commit()
+    db.refresh(position_in_db)
+
+    return {"Mensaje":"El cargo fue creado correctamente"}
---
+++ api/routers/service_order_detail_router.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, APIRouter, HTTPException
+from sqlalchemy.orm import Session
+from api.db.db_connection import get_db
+from api.db.service_order_detail_db import ServiceOrderDetailInDB
+from api.models.service_order_detail_models import ServiceOrderDetailIn, ServiceOrderDetailOut
+
+router = APIRouter()
+
+@router.get("/detail/{detail_id}", response_model=ServiceOrderDetailOut)
+async def get_detail(detail_id: int, db:Session=Depends(get_db)):
+    detail_in_db = db.query(ServiceOrderDetailIn).get(detail_id)
+    if detail_in_db == None:
+        raise HTTPException(status_code=404, detail="Detalle no encontrado")
+    return detail_in_db
+
+@router.post("/detail/create/")
+async def new_service_order_detail(order_service_detail_in:ServiceOrderDetailIn, db:Session=Depends(get_db)):
+
+    service_order_detail_in_db = ServiceOrderDetailInDB(**order_service_detail_in.dict())
+
+    db.add(service_order_detail_in_db)
+    db.commit()
+    db.refresh(service_order_detail_in_db)
+    
+    return {"Mensaje": "Detalle creado correctamente."}
