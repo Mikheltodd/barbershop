e0209ee4431b5bfae2fb22fba1a130a1c1e14698
Luis Palacio <luispalacio11c@gmail.com>
2021-01-07 11:06:09 -0500

    serviceorder get

--- api/routers/serviceorder_router.py
+++ api/routers/serviceorder_router.py
@@ -7,3 +7,9 @@ from models.serviceorder_models import ServiceorderIn, ServiceorderOut
 
 router = APIRouter()
 
+@router.get("/serviceorder/{serviceorder_id}", response_model=ServiceorderOut)
+async def get_serviceorder(serviceorder_id: str, db: Session = Depends(get_db)):
+    serviceorder_in_db = db.query(ServiceorderInDB).get(serviceorder_id)
+    if serviceorder_in_db == None:
+        raise HTTPException(status_code=404, detail="El id no existe")
+    return serviceorder_in_db
