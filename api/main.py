7765a06fd6e41480773f8aab1b2bb1d1a117a469
Luis Palacio <luispalacio11c@gmail.com>
2021-01-09 10:59:36 -0500

    Creacion tablas discounts and service_orders

--- api/main.py
+++ api/main.py
@@ -4,11 +4,11 @@ from routers.appointment_router import router as router_appointment
 # from routers.bill_router import router as router_bill
 from routers.category_router import router as router_category
 from routers.customer_router import router as router_customer
-# from routers.discount_router import router as router_discount
+from routers.discount_router import router as router_discount
 # from routers.employee_router import router as router_employee
 # from routers.position_router import router as router_position
 # from routers.service_order_detail_router import router as router_service_order_detail
-# from routers.service_order_router import router as router_service_order
+from routers.service_order_router import router as router_service_order
 from routers.service_router import router as router_service
 
 api = FastAPI()
@@ -18,9 +18,9 @@ api.include_router(router_appointment)
 # api.include_router(router_bill)
 api.include_router(router_category)
 api.include_router(router_customer)
-# api.include_router(router_discount)
+api.include_router(router_discount)
 # api.include_router(router_employee)
 # api.include_router(router_position)
 # api.include_router(router_service_order_detail)
-# api.include_router(router_service_order)
+api.include_router(router_service_order)
 api.include_router(router_service)
