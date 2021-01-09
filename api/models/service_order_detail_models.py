45a86268e17255b9613c2ce6a006b5a9e9e2e3bf
eslunap19 <estefany.luna@gmail.com>
2021-01-09 11:40:50 -0500

    update models & postman collection V1

--- api/models/service_order_detail_models.py
+++ api/models/service_order_detail_models.py
@@ -2,20 +2,20 @@ from pydantic import BaseModel
 
 
 class ServiceOrderDetailIn(BaseModel):
-    service_order_detail_service_id = int
-    service_order_detail_quantity = int
-    service_order_detail_amount = int
-    service_order_detail_service_order_id = int
-    service_order_detail_employee_id = int
+    service_order_detail_service_id : int
+    service_order_detail_quantity : int
+    service_order_detail_amount : int
+    service_order_detail_service_order_id : int
+    service_order_detail_employee_id : int
 
 
 class ServiceOrderDetailOut(BaseModel):
-    service_order_detail_id = int
-    service_order_detail_service_id = int
-    service_order_detail_quantity = int
-    service_order_detail_amount = int
-    service_order_detail_service_order_id = int
-    service_order_detail_employee_id = int
+    service_order_detail_id : int
+    service_order_detail_service_id : int
+    service_order_detail_quantity : int
+    service_order_detail_amount : int
+    service_order_detail_service_order_id : int
+    service_order_detail_employee_id : int
 
     class Config:
         orm_mode = True
