59ac40d3ba8651860c8ea7c4bf53452f02260529
eslunap19 <estefany.luna@gmail.com>
2021-01-09 10:55:06 -0500

    creation tables employees & position
    Creation succesfully

--- api/main.py
+++ api/main.py
@@ -5,8 +5,8 @@ from routers.appointment_router import router as router_appointment
 # from routers.category_router import router as router_category
 from routers.customer_router import router as router_customer
 # from routers.discount_router import router as router_discount
-# from routers.employee_router import router as router_employee
-# from routers.position_router import router as router_position
+from routers.employee_router import router as router_employee
+from routers.position_router import router as router_position
 # from routers.service_order_detail_router import router as router_service_order_detail
 # from routers.service_order_router import router as router_service_order
 # from routers.service_router import router as router_service
@@ -19,8 +19,8 @@ api.include_router(router_appointment)
 # api.include_router(router_category)
 api.include_router(router_customer)
 # api.include_router(router_discount)
-# api.include_router(router_employee)
-# api.include_router(router_position)
+api.include_router(router_employee)
+api.include_router(router_position)
 # api.include_router(router_service_order_detail)
 # api.include_router(router_service_order)
 # api.include_router(router_service)
