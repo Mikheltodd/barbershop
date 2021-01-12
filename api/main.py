146fa48973c530b92cd8516e0a2516e4226b2407
Mikheltodd <68166999+Mikheltodd@users.noreply.github.com>
2021-01-12 16:35:58 -0500

    Review Appointment Details

--- api/main.py
+++ api/main.py
@@ -1,13 +1,10 @@
 from fastapi import Depends, FastAPI
-# from routers.appointment_detail_router import router as router_appointment_detail
+from routers.appointment_detail_router import router as router_appointment_detail
 from routers.appointment_router import router as router_appointment
 # from routers.bill_router import router as router_bill
 from routers.category_router import router as router_category
 from routers.customer_router import router as router_customer
 from routers.discount_router import router as router_discount
-# from routers.employee_router import router as router_employee
-# from routers.position_router import router as router_position
-# from routers.discount_router import router as router_discount
 from routers.employee_router import router as router_employee
 from routers.position_router import router as router_position
 # from routers.service_order_detail_router import router as router_service_order_detail
@@ -16,18 +13,14 @@ from routers.service_router import router as router_service
 
 api = FastAPI()
 
-# api.include_router(router_appointment_detail)
+api.include_router(router_appointment_detail)
 api.include_router(router_appointment)
 # api.include_router(router_bill)
 api.include_router(router_category)
 api.include_router(router_customer)
-# api.include_router(router_discount)
-api.include_router(router_employee)
-# api.include_router(router_position)
 api.include_router(router_discount)
+api.include_router(router_employee)
 api.include_router(router_position)
-# api.include_router(router_employee)
-
 # api.include_router(router_service_order_detail)
 api.include_router(router_service_order)
 api.include_router(router_service)
